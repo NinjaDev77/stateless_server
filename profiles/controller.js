4627114c754634cddf412da54922b9eb373c3467
tier5 <work@tier5.us>
2017-10-04 19:41:38 +0530

    profile crud is completed

---
+++ profiles/controller.js
@@ -0,0 +1,213 @@
+'use strict';
+
+const moment            = require('moment');
+
+const uuid              = require('uuid/v4');
+const isE164PhoneNumber = require('is-e164-phone-number');
+const doc               = require('dynamodb-doc');
+const AWS               = require('aws-sdk');
+
+
+const dynamo            = new AWS.DynamoDB.DocumentClient();
+
+// function to get profile
+module.exports.getProfile=function(event,context){
+
+  let req           = event;
+  let body          = JSON.parse(req.body);
+  let params        = req.pathParameters;
+  let phoneNumber   = params.phoneNumber || event.params ;
+
+  if (phoneNumber === undefined ) {
+
+    context.succeed({code : 404 , message : 'No number was provided'});
+
+  } else {
+
+    var payload        = {
+      TableName: "profile",
+      KeyConditionExpression: "phoneNumber = :phn",
+      ExpressionAttributeValues: {
+        ":phn": phoneNumber
+      }
+    };
+
+    // function to get profile in dynamodb with the above params
+    dynamo.query(payload, function(err, data) {
+      if (err) {
+        console.error(err);
+        context.succeed({code : 400 , message : 'Error in getting profile'});
+
+      } else {
+
+        if (data.Items.length === 0 ) {
+
+          context.succeed({code : 200 , message : "No profile found !"});
+
+        } else {
+
+          context.succeed({code:200 , message : "OK" , data: data.Items});
+          
+        }
+
+      }
+
+    });
+  }
+
+};
+
+// function to create profile
+module.exports.createProfile = function(event,context){
+  let req                 = event;
+
+  let body                = JSON.parse(req.body);
+  let params              = req.pathParameters;
+
+  let phoneNumber         = params.phoneNumber || event.params ;
+
+  let customerID          = body.customerID,
+  customerType        = body.customerType,
+  customerEmail       = body.customerEmail,
+  customerMobile      = body.customerMobile,
+  uriAudioWelcome     = body.uriAudioWelcome,
+  uriAudioAppointment = body.uriAudioAppointment,
+  routingActive       = body.routingActive,
+  routingPhone        = body.routingActive;
+
+  var payload = {
+    TableName: 'profile',
+    Item: { // a map of attribute name to AttributeValue
+
+      "phoneNumber": phoneNumber,
+      "customerType":body.customerType,
+      "customerID"  :body.customerID,
+      "customerEmail":body.customerEmail,
+      "customerMobile":body.customerMobile,
+      "uriAudioWelcome":body.uriAudioWelcome,
+      "uriAudioAppointment":body.uriAudioAppointment,
+      "routingActive":body.routingActive,
+      "routingPhone":body.routingPhone,
+      "dateTimeCreated":moment().toISOString(),
+      "dateTimeUpdated":moment().toISOString()
+
+    },
+    ReturnValues: 'NONE', // optional (NONE | ALL_OLD)
+    ReturnConsumedCapacity: 'NONE', // optional (NONE | TOTAL | INDEXES)
+    ReturnItemCollectionMetrics: 'NONE', // optional (NONE | SIZE)
+  };
+  //method to put into the dynamodb
+  dynamo.put(payload, function(err, data) {
+    if (err) {
+
+      console.error(err); // an error occurred
+      context.succeed({code:400,message :"error"});
+
+    } else {
+
+      //console.log(JSON.stringify(data));
+      context.succeed({code:200,Description :"OK"});
+
+    } // successful response
+  });
+
+}
+
+//function to delete profile
+module.exports.deleteProfile = function(event, context){
+
+  let req         = event;
+
+  let body        = JSON.parse(req.body);
+  let params      = req.pathParameters;
+
+  let phoneNumber = params.phoneNumber || event.params ;
+
+
+  let payloads = {
+    TableName: "profile",
+    Key:{
+      "phoneNumber":phoneNumber
+    }
+  }
+  // function to delete profile in dynamodb
+  dynamo.delete(payloads, function(err, data) {
+    if (err) {
+
+      console.error("Unable to delete item. Error JSON:", JSON.stringify(err, null, 2));
+      context.succeed({code:400,message :"error"});
+
+    } else {
+
+      //console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+      context.succeed({code:200,Description :"OK"});
+
+    }
+
+  });
+}
+
+// function to update profile
+module.exports.updateProfile = function(event, context){
+
+  let req                 = event;
+  let body                = JSON.parse(req.body);
+  let params              = req.pathParameters;
+  let phoneNumber         = params.phoneNumber || event.params ;
+  let customerID          = body.customerID ;
+  let customerType        = body.customerType ;
+  let customerEmail       = body.customerEmail ;
+  let customerMobile      = body.customerMobile ;
+  let uriAudioWelcome     = body.uriAudioWelcome ;
+  let uriAudioAppointment = body.uriAudioAppointment ;
+  let routingActive       = (body.routingActive === true ? body.routingActive : undefined);
+  let routingPhone        = (body.routingActive === true ? body.routingPhone : undefined);
+
+  // object to map to the db attribute value
+  let dbAttributeValue      = {
+    ":cusId"    : customerID,
+    ":cusType"  : customerType,
+    ":cusEmail" : customerEmail,
+    ":cusMobile": customerMobile,
+    ":uriAudWel": uriAudioWelcome,
+    ":uriAudApp": uriAudioAppointment,
+    ":routAct"  : routingActive,
+    ":routPhn"  : routingPhone,
+    ":dtU"      : moment().toISOString()
+  };
+
+  // creating dynamic query string to map the Attribute values
+  let dbUpdateExpression  = "set "                                                              +
+  (customerID          ? "customerID           = :cusId, "     : "") +
+  (customerType        ? "customerType         = :cusType, "   : "") +
+  (customerEmail       ? "customerEmail        = :cusEmail, "  : "") +
+  (customerMobile      ? "customerMobile       = :cusMobile, " : "") +
+  (uriAudioWelcome     ? "uriAudioWelcome      = :uriAudWel, " : "") +
+  (uriAudioAppointment ? "uriAudioAppointment  = :uriAudApp, " : "") +
+  (routingActive       ? "routingActive        = :routAct, "   : "") +
+  (routingActive       ? "routingPhone         = :routPhn, "   : "") + "dateTimeUpdated = :dtU";
+
+  let payloads      = {
+    TableName: "profile",
+    Key:{
+      "phoneNumber":phoneNumber
+    },
+    UpdateExpression :dbUpdateExpression,
+    ExpressionAttributeValues:dbAttributeValue,
+    ReturnValues:"UPDATED_NEW"
+  };
+  console.log(payloads);
+  dynamo.update(payloads, function(err, data) {
+    if (err) {
+
+      console.error("Unable to update item. Error JSON:", JSON.stringify(err, null, 2));
+      context.succeed({code:400,message :err.message});
+
+    } else {
+      //console.log("DeleteItem succeeded:", JSON.stringify(data, null, 2));
+      context.succeed({code:200,Description :"OK"});
+    }
+
+  });
+
+}
---
+++ profiles/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const isE164PhoneNumber = require('is-e164-phone-number');
+
+const controller = require('./controller.js');
+
+
+/**
+* Demonstrates a simple HTTP endpoint using API Gateway. You have full
+* access to the request and response payload, including headers and
+* status code.
+*
+* To scan a DynamoDB table, make a GET request with the TableName as a
+* query string parameter. To put, update, or delete an item, make a POST,
+* PUT, or DELETE request respectively, passing in the payload to the
+* DynamoDB API as a JSON body.
+*/
+exports.handler = (event, context, callback) => {
+  //console.log('Received event:', JSON.stringify(event, null, 2));
+
+  switch (event.httpMethod) {
+
+    case 'GET':
+    // dynamo.scan({ TableName: event.queryStringParameters.TableName }, done);
+      controller.getProfile(event, context);
+      break;
+
+    case 'POST':
+      controller.createProfile(event, context);
+      break;
+
+    case 'DELETE':
+      controller.deleteProfile(event, context);
+      break;
+      
+    case 'PUT':
+      controller.updateProfile(event, context);
+      break;
+
+    default:
+      done(new Error(`Unsupported method "${event.httpMethod}"`));
+  }
+};
